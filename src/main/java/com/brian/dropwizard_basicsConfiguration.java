42edb39fa8bbbd8e5f40d95aee28ec362f87e4b8
Brian Moon <brian.dhmoon@gmail.com>
2020-02-15 09:26:35 +1100

    basic dropwizard skeleton

---
+++ src/main/java/com/brian/dropwizard_basicsConfiguration.java
@@ -0,0 +1,10 @@
+package com.brian;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class dropwizard_basicsConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
